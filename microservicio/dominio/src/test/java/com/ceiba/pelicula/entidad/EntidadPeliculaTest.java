def4fd324f4792a863c9fab7a78d65b24eb06d75
Anderson Camacho Palacios - Ceiba Software <anderson.camacho@ceiba.com.co>
2021-09-22 17:57:22 -0500

    Modificaciones al Test Unitario de Peliculo- anexo de pruebas en la entidad

---
+++ microservicio/dominio/src/test/java/com/ceiba/pelicula/entidad/EntidadPeliculaTest.java
@@ -0,0 +1,33 @@
+package com.ceiba.pelicula.entidad;
+
+import com.ceiba.BasePrueba;
+import com.ceiba.dominio.ValidadorArgumento;
+import com.ceiba.dominio.excepcion.ExcepcionSinDatos;
+import com.ceiba.dominio.excepcion.ExcepcionValorObligatorio;
+import com.ceiba.pelicula.modelo.entidad.Pelicula;
+import com.ceiba.pelicula.servicio.testdatabuilder.PeliculaTestDataBuilder;
+import org.junit.Test;
+
+public class EntidadPeliculaTest {
+
+    private static final String SE_DEBE_INGRESAR_EL_NOMBRE_DEL_DIRECTOR = "Se debe ingresar el nombre del director";
+    private static final String SE_DEBE_INGRESAR_EL_TITULO_DE_LA_PELICULA = "Se debe ingresar el titulo de la pelicula";
+
+
+    @Test
+    public void validarObligatorioTituloPelicula(){
+        //Arrange
+        PeliculaTestDataBuilder peliculaTestDataBuilder = new PeliculaTestDataBuilder().sinTitulo();
+
+        //Act - Assert
+        BasePrueba.assertThrows(()-> peliculaTestDataBuilder.build(), ExcepcionValorObligatorio.class, SE_DEBE_INGRESAR_EL_TITULO_DE_LA_PELICULA);
+    }
+    @Test
+    public void validarObligatorioDirectorPelicula(){
+        //Arrange
+        PeliculaTestDataBuilder peliculaTestDataBuilder = new PeliculaTestDataBuilder().sinDirector();
+
+        //Act - Assert
+        BasePrueba.assertThrows(()-> peliculaTestDataBuilder.build(), ExcepcionValorObligatorio.class, SE_DEBE_INGRESAR_EL_NOMBRE_DEL_DIRECTOR);
+    }
+}
--- microservicio/dominio/src/test/java/com/ceiba/pelicula/servicio/testdatabuilder/PeliculaTestDataBuilder.java
+++ microservicio/dominio/src/test/java/com/ceiba/pelicula/servicio/testdatabuilder/PeliculaTestDataBuilder.java
@@ -5,18 +5,31 @@ import com.ceiba.pelicula.modelo.entidad.Pelicula;
 public class PeliculaTestDataBuilder {
     private Long id;
     private String titulo;
-    private String Director;
+    private String director;
 
     public PeliculaTestDataBuilder() {
         this.titulo = "Rapidos_y_Furiosos";
-        Director = "Volado_De_La_Cabeza";
+        director = "Volado_De_La_Cabeza";
     }
 
     public PeliculaTestDataBuilder conId(Long id){
         this.id = id;
         return this;
     }
+
+    public PeliculaTestDataBuilder sinTitulo( ){
+        this.titulo = null;
+        return this;
+    }
+
+
+    public PeliculaTestDataBuilder sinDirector( ){
+        this.director = null;
+        return this;
+    }
+
+
     public Pelicula build(){
-        return new Pelicula(this.id, this.titulo, this.Director);
+        return new Pelicula(this.id, this.titulo, this.director);
     }
 }
