28409d4095b361850bdd9ccd75f1459bff4b5e9b
Anderson Camacho <anderson_camacho@outlook.com>
2021-10-11 11:47:11 -0500

    reparacion de test

--- microservicio/dominio/src/test/java/com/ceiba/usuario/servicio/ServicioCrearUsuarioTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/usuario/servicio/ServicioCrearUsuarioTest.java
@@ -4,6 +4,7 @@ import com.ceiba.usuario.modelo.entidad.Usuario;
 import com.ceiba.usuario.puerto.repositorio.RepositorioUsuario;
 import com.ceiba.dominio.excepcion.ExcepcionDuplicidad;
 import com.ceiba.usuario.testdatabuilder.UsuarioTestDataBuilder;
+import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -20,6 +21,18 @@ public class ServicioCrearUsuarioTest {
         Mockito.when(repositorioUsuario.existe(Mockito.anyLong())).thenReturn(true);
         ServicioCrearUsuario servicioCrearUsuario = new ServicioCrearUsuario(repositorioUsuario);
         // act - assert
+        BasePrueba.assertThrows(() -> servicioCrearUsuario.ejecutar(usuario), ExcepcionDuplicidad.class,"El usuario ya existe en el sistema");
+    }
+
+    @Test
+    public void validarUsuarioNoExistenciaPreviaTest() {
+        // arrange
+        Usuario usuario = new UsuarioTestDataBuilder().build();
+        RepositorioUsuario repositorioUsuario = Mockito.mock(RepositorioUsuario.class);
+        Mockito.when(repositorioUsuario.existe(Mockito.anyLong())).thenReturn(false);
+        ServicioCrearUsuario servicioCrearUsuario = new ServicioCrearUsuario(repositorioUsuario);
+        // act - assert
+        Assert.assertNotNull(servicioCrearUsuario.ejecutar(usuario));
         BasePrueba.assertThrows(() -> servicioCrearUsuario.ejecutar(usuario), ExcepcionDuplicidad.class,"El usuario ya existe en el sistema");
     }
 }
