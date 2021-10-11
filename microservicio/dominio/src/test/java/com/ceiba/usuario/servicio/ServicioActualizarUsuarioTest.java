94cd9306c64a71267b5aca7d0e35ea19a9584eaf
Anderson Camacho <anderson_camacho@outlook.com>
2021-10-11 11:44:41 -0500

    reparacion de test

--- microservicio/dominio/src/test/java/com/ceiba/usuario/servicio/ServicioActualizarUsuarioTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/usuario/servicio/ServicioActualizarUsuarioTest.java
@@ -7,6 +7,7 @@ import com.ceiba.usuario.modelo.entidad.Usuario;
 import com.ceiba.usuario.puerto.repositorio.RepositorioUsuario;
 import com.ceiba.usuario.testdatabuilder.UsuarioTestDataBuilder;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.mockito.Mockito;
 
 public class ServicioActualizarUsuarioTest {
@@ -14,7 +15,7 @@ public class ServicioActualizarUsuarioTest {
     private static final String EL_USUARIO_NO_EXISTE_EN_EL_SISTEMA = "El usuario no existe en el sistema";
 
     @Test
-    public void validarUsuarioExistenciaPreviaTest() {
+    public void validarUsuarioNoExistenciaPreviaTest() {
         // arrange
         Usuario usuario = new UsuarioTestDataBuilder().conId(1L).build();
         RepositorioUsuario repositorioUsuario = Mockito.mock(RepositorioUsuario.class);
@@ -22,6 +23,19 @@ public class ServicioActualizarUsuarioTest {
         ServicioActualizarUsuario servicioActualizarUsuario = new ServicioActualizarUsuario(repositorioUsuario);
         // act - assert
         BasePrueba.assertThrows(() -> servicioActualizarUsuario.ejecutar(usuario), ExcepcionUsuarioNoExiste.class, EL_USUARIO_NO_EXISTE_EN_EL_SISTEMA);
+    }
+
+    @Test
+    public void validarUsuarioExistenciaPreviaTest() {
+        // arrange
+        Usuario usuario = new UsuarioTestDataBuilder().conId(1L).build();
+        RepositorioUsuario repositorioUsuario = Mockito.mock(RepositorioUsuario.class);
+        Mockito.when(repositorioUsuario.existe(Mockito.anyLong())).thenReturn(true);
+        ServicioActualizarUsuario servicioActualizarUsuario = new ServicioActualizarUsuario(repositorioUsuario);
+        // act - assert
+        Assertions.assertDoesNotThrow(()->servicioActualizarUsuario.ejecutar(usuario));
+        Mockito.verify(repositorioUsuario, Mockito.times(1)).actualizar(usuario);
+//        BasePrueba.assertThrows(() -> servicioActualizarUsuario.ejecutar(usuario), ExcepcionUsuarioNoExiste.class, EL_USUARIO_NO_EXISTE_EN_EL_SISTEMA);
     }
 
 }
